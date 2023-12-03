7295517e3a25dabe6faee332ea0e044d38120ad6
NicolasMontone <montonenicolas01@gmail.com>
2023-12-02 23:46:51 -0300

    fix: chart data string to number.

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -5,13 +5,17 @@ import { LineChart, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts'
 export default function Chart({ n, data }) {
 	return (
 		<>
-			<LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-				<XAxis dataKey="name" />
-				<YAxis />
-				<Tooltip />
-				<Legend />
-				<Line type="monotone" dataKey={`EEG ${n}`} stroke={`#${n}884d8`} dot={false} />
+			<div
+				style={{ width: "100%", height: "250px" }}
+			>
+				<LineChart width={1300} height={250} data={data.map((data) => ({ ...data, [`EEG ${n}`]: Number(data[`EEG ${n}`]) }))} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+					<XAxis dataKey="name" />
+					<YAxis />
+					<Tooltip />
+					<Legend />
+					<Line type="monotone" dataKey={`EEG ${n}`} stroke={`#${n}884d8`} dot={false} />
 
-			</LineChart>
+				</LineChart>
+			</div >
 		</>)
 }
