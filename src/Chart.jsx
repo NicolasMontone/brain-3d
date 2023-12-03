18904b81e90996c09de221bf080c1cd41b8a7efc
NicolasMontone <montonenicolas01@gmail.com>
2023-12-02 23:55:48 -0300

    chore: nit

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -6,7 +6,10 @@ export default function Chart({ n, data }) {
 	return (
 		<>
 			<div
-				style={{ width: "100%", height: "250px" }}
+				style={{
+					width: "100%",
+					height: "250px"
+				}}
 			>
 				<LineChart width={1300} height={250} data={data.map((data) => ({ ...data, [`EEG ${n}`]: Number(data[`EEG ${n}`]) }))} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
 					<XAxis dataKey="name" />
